993d92243dad1ce7fed3e155fdb08d1f5aa6f4fa
baysik <iambaysik@gmail.com>
2019-08-03 16:26:39 -0600

    conditional statements added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,36 +11,45 @@ let connection = mysql.createConnection({
 
 console.log('Current items in bamazon database');
 connection.query("SELECT * FROM products", function(err, res){
+    let chosenItem;
     if (err) throw err;
     for (var i=0; i < res.length; i++){
+        chosenItem = res[i];
         console.log(res[i]);
     }
-})
-inquirer.prompt([
-    {
-        type: "input",
-        name: "productChoice",
-        message: "What item would you like to purchase? (type the ID number)"
-    },
-    {
-        type: "input",
-        name: "purchaseAmount",
-        message: "How many would you like to purchase?"
-    }
-]).then(answers => {
-    console.log(answers.productChoice, answers.purchaseAmount);
-        "UPDATE products SET ? WHERE ?",
-        [
-            {
-                id: answers.productChoice
-            },
-            {
-                stock_quantity: (answers.purchaseAmount - stock_quantity)
-            }
-        ],
-        function (err, res) {
-            if (err) throw err;
-            console.log(`${res.affectedRows} purchased!\n`);
-            connection.end();
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "productChoice",
+            message: "What item would you like to purchase? (type the ID number)"
+        },
+        {
+            type: "input",
+            name: "purchaseAmount",
+            message: "How many would you like to purchase?"
         }
+    ]).then(answers => {
+        console.log(answers.productChoice, answers.purchaseAmount);
+        console.log('updating database...');
+        if (parseInt(answers.purchaseAmount < chosenItem.stock_quantity)) {
+            connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: answers.purchaseAmount
+                    },
+                    {
+                        id: answers.productChoice
+                    }
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(`${res.affectedRows} purchased!\n`);
+                    connection.end();
+                }
+            )
+            console.log(answers)
+
+        }
+    })
 })
